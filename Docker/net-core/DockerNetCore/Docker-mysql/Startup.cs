669b933d0b5d727e7412f10072ccdec3609f208c
jose.fernandez <jose.fernandez@ceiba.com.co>
2019-10-07 21:55:26 -0500

    more yamls and sh*t

--- Docker/net-core/DockerNetCore/Docker-mysql/Startup.cs
+++ Docker/net-core/DockerNetCore/Docker-mysql/Startup.cs
@@ -17,6 +17,7 @@ namespace Docker_mysql
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+            
         }
 
         public IConfiguration Configuration { get; }
